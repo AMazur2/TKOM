33886cfaaa7939214128dcaf6ee3789ae345fccc
AMazur2 <45732226+AMazur2@users.noreply.github.com>
2021-04-20 13:15:21 +0200

    Update Tokens.java

--- src/Lexer/Tokens.java
+++ src/Lexer/Tokens.java
@@ -2,10 +2,9 @@ package Lexer;
 
 public enum Tokens {
     NUM , MESS , BOOL , REF ,                                       // typy zmiennych
-    TRUE , FALSE ,
     ADD_OP , MULT_OP , MOD_OP , POW_OP,                             // działania matematyczne
     COMPARISON_OP , ASSIGNMENT_OP , OR_OP , AND_OP ,  NOT_OP ,      // operatory logiczne
-    STRING , INT , CLASS , RETURN , IDENTIFIER ,
+    STRING , INT , CLASS , BOOLEAN , RETURN , IDENTIFIER ,
     COLON , SEMICOLON , BAR , DOLLAR, FLOOR ,
     ROUND_OPEN , ROUND_CLOSE , CURL_OPEN , CURL_CLOSE ,             // nawiasy
     IF , ELSEIF , ELSE,  WHILE , FOR ,
